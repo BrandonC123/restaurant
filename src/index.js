d4e99302e8e091277f1a7448c2c9251a129ebe93
Brandon Chu <brandoc318@gmail.com>
2022-01-29 18:54:14 -0800

    Starting to figure out logic to switch tabs.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
-const setup = (() => {
-    let activePage;
-    frontPage();
-    const homeBtn = document.getElementById("home");
-    homeBtn.addEventListener("click", () => {});
-    const menuBtn = document.getElementById("menu");
-    menuBtn.addEventListener("click", () => {
-        menuPage();
-    });
-    const contactBtn = document.getElementById("contact");
-    homeBtn.addEventListener("click", () => {});
-    const textCont = document.createElement("div");
-    textCont.classList.add("text-container");
-    return {
-        activePage,
-        textCont,
-    };
-})();
-
-function frontPage() {
+const frontPage = (() => {
     const header = document.createElement("div");
     header.classList.add("header");
 
@@ -40,97 +21,157 @@ function frontPage() {
     header.appendChild(menu);
     header.appendChild(contact);
 
-    // const title = document.createElement("div");
-    // title.textContent = "Burger Shop";
-    // title.setAttribute("id", "title");
+    function createHome() {
+        const title = document.createElement("div");
+        title.textContent = "Burger Shop";
+        title.setAttribute("id", "title");
 
-    // const summary = document.createElement("div");
-    // summary.textContent = `Lorem ipsum dolor sit amet, consectetur
-    // adipiscing elit. Integer venenatis ornare dolor vitae aliquet.
-    // Curabitur vestibulum posuere urna, pulvinar tempus lacus cursus at.
-    // Pellentesque purus nisi, porta et nulla eget, hendrerit
-    // accumsan ligula. Sed at turpis porttitor, egestas lacus ac,
-    // fringilla tellus.`;
-    // summary.setAttribute("id", "sum");
+        const summary = document.createElement("div");
+        summary.textContent = `Lorem ipsum dolor sit amet, consectetur
+    adipiscing elit. Integer venenatis ornare dolor vitae aliquet.
+    Curabitur vestibulum posuere urna, pulvinar tempus lacus cursus at.
+    Pellentesque purus nisi, porta et nulla eget, hendrerit
+    accumsan ligula. Sed at turpis porttitor, egestas lacus ac,
+    fringilla tellus.`;
+        summary.setAttribute("id", "sum");
 
-    // const menu1 = document.createElement("div");
-    // menu1.classList.add("menu-item");
-    // menu1.setAttribute("id", "menu1");
-    // menu1.textContent = "fill";
+        const menu1 = document.createElement("div");
+        menu1.classList.add("menu-item");
+        menu1.setAttribute("id", "menu1");
+        menu1.textContent = "fill";
 
-    // const menu2 = document.createElement("div");
-    // menu2.classList.add("menu-item");
-    // menu2.setAttribute("id", "menu2");
-    // menu2.textContent = "fill";
+        const menu2 = document.createElement("div");
+        menu2.classList.add("menu-item");
+        menu2.setAttribute("id", "menu2");
+        menu2.textContent = "fill";
 
-    // const menu3 = document.createElement("div");
-    // menu3.classList.add("menu-item");
-    // menu3.setAttribute("id", "menu3");
-    // menu3.textContent = "fill";
+        const menu3 = document.createElement("div");
+        menu3.classList.add("menu-item");
+        menu3.setAttribute("id", "menu3");
+        menu3.textContent = "fill";
 
-    // const location = document.createElement("div");
-    // location.setAttribute("id", "location");
-    // location.textContent = "123 Sesame Street";
+        const location = document.createElement("div");
+        location.setAttribute("id", "location");
+        location.textContent = "123 Sesame Street";
 
-    // const hours = document.createElement("div");
-    // hours.setAttribute("id", "hours");
-    // hours.textContent = `Sunday: 10am - 10pm Saturday: 9am - 10pm
-    // Monday-Friday: 9am - 10pm Hours subject to change on holidays`;
+        const hours = document.createElement("div");
+        hours.setAttribute("id", "hours");
+        hours.textContent = `Sunday: 10am - 10pm Saturday: 9am - 10pm
+    Monday-Friday: 9am - 10pm Hours subject to change on holidays`;
 
-    // textCont.appendChild(title);
-    // textCont.appendChild(summary);
-    // textCont.appendChild(menu1);
-    // textCont.appendChild(menu2);
-    // textCont.appendChild(menu3);
-    // textCont.appendChild(location);
-    // textCont.appendChild(hours);
+        const homeContainer = document.createElement("div");
+        homeContainer.appendChild(title);
+        homeContainer.appendChild(summary);
+        homeContainer.appendChild(menu1);
+        homeContainer.appendChild(menu2);
+        homeContainer.appendChild(menu3);
+        homeContainer.appendChild(location);
+        homeContainer.appendChild(hours);
+    }
+    return {
+        createHome,
+        header,
+        homeContainer,
+    }
+})();
 
+const menu = () => {
+    function createMenu() {
+        const menuTitle = document.createElement("div");
+        menuTitle.textContent = "Menu";
+        menuTitle.classList.add("title");
+        setup.textCont.appendChild(menuTitle);
+
+        const burgName1 = document.createElement("div");
+        burgName1.textContent = "Burger 1";
+        burgName1.classList.add("food-name");
+        const burgImg1 = document.createElement("img");
+        burgImg1.src = "../dist/1.jpg";
+        burgImg1.classList.add("food-img");
+        const menuItem1 = document.createElement("div");
+        menuItem1.classList.add("menu-item");
+        menuItem1.appendChild(burgName1);
+        menuItem1.appendChild(burgImg1);
+        setup.textCont.appendChild(menuItem1);
+
+        const burgName2 = document.createElement("div");
+        burgName2.textContent = "Burger 2";
+        burgName2.classList.add("food-name");
+        const burgImg2 = document.createElement("img");
+        burgImg2.src = "../dist/2.jpg";
+        burgImg2.classList.add("food-img");
+        const menuItem2 = document.createElement("div");
+        menuItem2.classList.add("menu-item");
+        menuItem2.appendChild(burgName2);
+        menuItem2.appendChild(burgImg2);
+        setup.textCont.appendChild(menuItem2);
+
+        const burgName3 = document.createElement("div");
+        burgName3.textContent = "Burger 3";
+        burgName3.classList.add("food-name");
+        const burgImg3 = document.createElement("img");
+        burgImg3.src = "../dist/3.jpg";
+        burgImg3.classList.add("food-img");
+        const menuItem3 = document.createElement("div");
+        menuItem3.classList.add("menu-item");
+        menuItem3.appendChild(burgName3);
+        menuItem3.appendChild(burgImg3);
+        setup.textCont.appendChild(menuItem3);
+
+        const cont = document.getElementById("content");
+        cont.appendChild(setup.textCont);
+    }
+    return {
+        createMenu,
+    };
+};
+
+const contact = () => {
+    function createContact() {
+        const contactTitle = document.createElement("div");
+        contactTitle.classList.add("title");
+        contactTitle.textContent = "Contact";
+
+        const contactCont = document.createElement("div");
+        contactCont.classList.add("contact-cont");
+
+        const number = document.createElement("p");
+        number.textContent = "Phone Number: Insert Number";
+        const email = document.createElement("p");
+        email.textContent = "Email: realemail@gmail.com";
+        const location = document.createElement("p");
+        location.textContent = "Location: 123 Sesame Street";
+
+        contactCont.appendChild(number);
+        contactCont.appendChild(email);
+        contactCont.appendChild(location);
+    }
+    return {
+        createContact,
+    };
+};
+
+const setup = (() => {
+    let activePage;
     const content = document.getElementById("content");
-    content.appendChild(header);
-    // content.appendChild(textCont);
-}
-
-function menuPage() {
-    const menuTitle = document.createElement("div");
-    menuTitle.textContent = "Menu";
-    menuTitle.classList.add("title");
-    setup.textCont.appendChild(menuTitle);
-
-    const burgName1 = document.createElement("div");
-    burgName1.textContent = "Burger 1";
-    burgName1.classList.add("food-name");
-    const burgImg1 = document.createElement("img");
-    burgImg1.src = "../dist/1.jpg";
-    burgImg1.classList.add("food-img");
-    const menuItem1 = document.createElement("div");
-    menuItem1.classList.add("menu-item");
-    menuItem1.appendChild(burgName1);
-    menuItem1.appendChild(burgImg1);
-    setup.textCont.appendChild(menuItem1);
-
-    const burgName2 = document.createElement("div");
-    burgName2.textContent = "Burger 2";
-    burgName2.classList.add("food-name");
-    const burgImg2 = document.createElement("img");
-    burgImg2.src = "../dist/2.jpg";
-    burgImg2.classList.add("food-img");
-    setup.textCont.appendChild();
-    const menuItem2 = document.createElement("div");
-    menuItem2.classList.add("menu-item");
-    menuItem2.appendChild(burgName2);
-    menuItem2.appendChild(burgImg2);
-    setup.textCont.appendChild(menuItem2);
-
-    const burgName3 = document.createElement("div");
-    burgName3.textContent = "Burger 3";
-    burgName3.classList.add("food-name");
-    const burgImg3 = document.createElement("img");
-    burgImg3.src = "../dist/3.jpg";
-    burgImg3.classList.add("food-img");
-    setup.textCont.appendChild();
-    const menuItem3 = document.createElement("div");
-    menuItem3.classList.add("menu-item");
-    menuItem3.appendChild(burgName3);
-    menuItem3.appendChild(burgImg3);
-    setup.textCont.appendChild(menuItem3);
-}
+    const textCont = document.createElement("div");
+    textCont.classList.add("text-container");
+    content.appendChild(frontPage.header);
+    frontPage.createHome();
+    textCont.appendChild(frontPage.homeContainer);
+    const homeBtn = document.getElementById("home");
+    homeBtn.addEventListener("click", () => {
+        textCont.appendChild(frontPage.homeContainer);
+    });
+    const menuBtn = document.getElementById("menu");
+    menuBtn.addEventListener("click", () => {
+        menu.createMenu();
+    });
+    const contactBtn = document.getElementById("contact");
+    homeBtn.addEventListener("click", () => {});
+    return {
+        activePage,
+        content,
+        textCont,
+    };
+})();
